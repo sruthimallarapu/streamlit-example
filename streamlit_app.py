5b693160e37e32c2a3ca066aaf340dbbb4c3e408
sruthimallarapu <sruthimallarapu@gmail.com>
2022-10-31 19:52:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-st.sidebar.title('Smart Farm Surveillance')
-st.sidebar.subheader('Settings')
+st.sidebar.title('Smart Farm Surveillance System')
 
-base_genre = st.sidebar.selectbox("Choose your preference",('About Smart Farm Surveillance','Livestock Monitoring','Object Detection in Farm'))
+base_genre = st.sidebar.selectbox("Choose your preference",('About Smart Farm Surveillance System', 'Livestock Detection and Tracking', 'Intruder Detection and Tracking', 'Vehicle Detection and Tracking'))
 
 if base_genre == 'About Smart Farm Surveillance':
 	with st.container():
@@ -13,3 +12,35 @@ if base_genre == 'About Smart Farm Surveillance':
 		st.markdown('**Smart Farm Surveillance** system is an alternative to the traditional approach of farm monitoring by the use of technology-oriented agricultural techniques.\n')
 		st.markdown('The main of this surveillance system is to reduce the manual effort required for security of the farm. \n')
 		st.markdown('The current version of this Smart Farm Surveillance system performs the tasks of classifying the cattle & its behavior along with object detection by the use of deep learning models.\n')
+		
+elif base_genre == 'Livestock Detection and Tracking':
+	with st.container():
+		st.markdown("# Livestock Detection and Tracking")
+		st.markdown(''' Here the type of cattle is identified using the drone video of cattle grazing on the farmland. Cattles like cow, sheep and horse are identified.
+			            The basic positional behavior of cattles like standing, eating and idle states are  captured.
+			            Along with the mentioned classification, the cattles are counted and their movement is also tracked.
+			            The output classification video display either all the cattle types or the cattle type given by the user.
+                    ''')
+		genre = st.sidebar.radio('Choose the livestock classification type',('Species Classification','Behavior Classification'))
+
+	with st.container():
+		st.markdown("<h1 style='text-align: center; color: black;'>Livestock Monitoring</h1>", unsafe_allow_html=True)
+		st.markdown(''' Here the type of cattle is identified using the drone video of cattle grazing on the farmland. Cattles like cow, sheep and horse are identified.
+			            The basic positional behavior of cattles like standing, eating and idle states are  captured.
+			            Along with the mentioned classification, the cattles are counted and their movement is also tracked.
+			            The output classification video display either all the cattle types or the cattle type given by the user.
+                    ''')
+		genre = st.sidebar.radio('Choose the livestock classification type',('Species Classification','Behavior Classification'))
+
+		if genre == 'Species Classification':
+			st.markdown("<h3 style='text-align: center; color: black;'>Species Classification</h3>", unsafe_allow_html=True)
+			names = ['All','cow','sheep','horse']
+			assigned_class = st.sidebar.radio("Choose the cattle to be identified:", names)
+			st.sidebar.markdown('---')
+			uploaded_files = st.sidebar.file_uploader("Upload the drone video of farm", type=[ "mp4", "mov",'avi','asf', 'm4v' ], accept_multiple_files=False)
+		else:
+			st.markdown("<h3 style='text-align: center; color: black;'>Behavior Classification</h3>", unsafe_allow_html=True)
+			names = ['All','Stand','Eat','Idle']
+			assigned_class = st.sidebar.radio("Choose the behavior type to be identified:", names)
+			st.sidebar.markdown('---')
+			uploaded_files = st.sidebar.file_uploader("Upload the drone video of farm", type=[ "mp4", "mov",'avi','asf', 'm4v' ], accept_multiple_files=False)
