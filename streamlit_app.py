412401a2d890eebb319cd69ab55f252bd06bd0fa
sruthimallarapu <sruthimallarapu@gmail.com>
2022-10-31 20:04:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ st.sidebar.title('Smart Farm Surveillance System')
 
 base_genre = st.sidebar.selectbox("Choose your preference",('About Smart Farm Surveillance System', 'Livestock Detection and Tracking', 'Intruder Detection and Tracking', 'Vehicle Detection and Tracking'))
 
-if base_genre == 'About Smart Farm Surveillance':
+if base_genre == 'About Smart Farm Surveillance System':
 	with st.container():
-		st.markdown("# Smart Farm Surveillance")
+		st.markdown("<h1 style='text-align: center; color: black;'>Smart Farm Surveillance System</h1>", unsafe_allow_html=True")
 		#image = Image.open('Search-for-Lost-Cattle.jpeg')
 		#st.image(image)
 		st.markdown('**Smart Farm Surveillance** system is an alternative to the traditional approach of farm monitoring by the use of technology-oriented agricultural techniques.\n')
@@ -25,13 +25,13 @@ elif base_genre == 'Livestock Detection and Tracking':
 		
 		if genre == 'Species Classification':
 			st.markdown("<h3 style='text-align: center; color: black;'>Species Classification</h3>", unsafe_allow_html=True)
-			names = ['All','cow','sheep','horse']
+			names = ['All','Cow','Sheep','Horse']
 			assigned_class = st.sidebar.radio("Choose the cattle to be identified:", names)
 			st.sidebar.markdown('---')
-			#uploaded_files = st.sidebar.file_uploader("Upload the drone video of farm", type=[ "mp4", "mov",'avi','asf', 'm4v' ], accept_multiple_files=False)
+			uploaded_files = st.sidebar.file_uploader("Upload the drone video of farm", type=[ "mp4", "mov",'avi','asf', 'm4v' ], accept_multiple_files=False)
 		else:
 			st.markdown("<h3 style='text-align: center; color: black;'>Behavior Classification</h3>", unsafe_allow_html=True)
-			names = ['All','Stand','Eat','Idle']
+			names = ['All','Sit','Stand','Sleep']
 			assigned_class = st.sidebar.radio("Choose the behavior type to be identified:", names)
 			st.sidebar.markdown('---')
-			#uploaded_files = st.sidebar.file_uploader("Upload the drone video of farm", type=[ "mp4", "mov",'avi','asf', 'm4v' ], accept_multiple_files=False)
+			uploaded_files = st.sidebar.file_uploader("Upload the drone video of farm", type=[ "mp4", "mov",'avi','asf', 'm4v' ], accept_multiple_files=False)
