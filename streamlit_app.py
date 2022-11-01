af66d6f0f8448ab5405dbfd89aace141d6b29866
sruthimallarapu <sruthimallarapu@gmail.com>
2022-10-31 19:06:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.sidebar.title('Smart Farm Surveillance')
 st.sidebar.subheader('Settings')
+
+base_genre = st.sidebar.selectbox("Choose your preference",('About Smart Farm Surveillance','Livestock Monitoring','Object Detection in Farm'))
