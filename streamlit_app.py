c1af1d5c3bd754e2f99c2dd1a4d76e4dc0f1ae63
sruthimallarapu <sruthimallarapu@gmail.com>
2022-10-31 19:05:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-df
+
+st.sidebar.title('Smart Farm Surveillance')
+st.sidebar.subheader('Settings')
